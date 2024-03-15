d0ea6f41505138851c23abd7c9ce6c3ceb4610dd
devlrakib <devrakib5@gmail.com>
2024-03-15 12:24:08 +0600

    first commit

---
+++ break.js
@@ -0,0 +1,16 @@
+// for ( var i=0; i<10; i++){
+//     console.log(i);
+//     if (i>5){
+//         break;
+//     }
+// }
+
+var roastgiven = 0;
+while ( roastgiven < 10){
+    console.log("Types of while loop");
+    roastgiven++;
+    if (roastgiven > 4){
+        console.log("Types of");
+        break;
+    }
+}
---
+++ continue.js
@@ -0,0 +1,7 @@
+var  numbers = [2, 4, 6, 8, 10];
+for (var i=0; i<numbers.length; i++){
+    var numbers = numbers[i];
+    if (numbers > 2){
+        continue;
+    };
+}
